b24bb77727cc7ff1f2b081b9845c359ab998438c
Dylan Cross <dylnnzl@gmail.com>
2017-03-23 16:50:19 +1300

    Tidied up Puzzle.h/cpp

--- puzzle.cpp
+++ puzzle.cpp
@@ -7,37 +7,30 @@ using namespace std;
 //////////////////////////////////////////////////////////////
 //constructor
 //////////////////////////////////////////////////////////////
-Puzzle::Puzzle(const Puzzle &p) : path(p.path){
-
+Puzzle::Puzzle(const Puzzle &p) : path(p.path) {
 	for(int i=0; i < 3; i++){
 		for(int j=0; j < 3; j++){
 		    board[i][j] = p.board[i][j];
 		    goalBoard[i][j] = p.goalBoard[i][j];
 		}
 	}
-
 	x0 = p.x0;
 	y0 = p.y0;
-	//path = p.path;
 	pathLength = p.pathLength;
 	hCost = p.hCost;
 	fCost = p.fCost;
 	strBoard = toString(); //uses the board contents to generate the string equivalent
 	depth = p.depth;
-
 }
 
 //////////////////////////////////////////////////////////////
 //constructor
 //inputs:  initial state, goal state
 //////////////////////////////////////////////////////////////
-Puzzle::Puzzle(string const elements, string const goal){
-
-	int n;
-
-	n = 0;
-	for(int i=0; i < 3; i++){
-		for(int j=0; j < 3; j++){
+Puzzle::Puzzle(string const elements, string const goal) {
+	int n = 0;
+	for(int i=0; i < 3; i++) {
+		for(int j=0; j < 3; j++) {
 		    board[i][j] = elements[n] - '0';
 		    if(board[i][j] == 0){
 			    x0 = j;
@@ -46,7 +39,6 @@ Puzzle::Puzzle(string const elements, string const goal){
 		    n++;
 		}
 	}
-
 	///////////////////////
 	n = 0;
 	for(int i=0; i < 3; i++){
@@ -65,236 +57,192 @@ Puzzle::Puzzle(string const elements, string const goal){
 }
 
 
-void Puzzle::setDepth(int d){
+void Puzzle::setDepth(int d) {
 	depth = d;
 }
 
-int Puzzle::getDepth(){
+int Puzzle::getDepth() {
 	return depth;
 }
 
-void Puzzle::updateHCost(heuristicFunction hFunction){
+void Puzzle::updateHCost(heuristicFunction hFunction) {
 	hCost = h(hFunction);
 }
 
-void Puzzle::updateFCost(){
+void Puzzle::updateDepth(){
+ depth++;
+}
+
+void Puzzle::updateFCost() {
 	//fCost = ?
 }
 
-int Puzzle::getFCost(){
+int Puzzle::getFCost() {
 	return fCost;
 }
 
-int Puzzle::getHCost(){
+int Puzzle::getHCost() {
 	return hCost;
 }
 
-int Puzzle::getGCost(){
+int Puzzle::getGCost() {
 	return pathLength;
 }
 
+// TODO: A*
 //Heuristic function implementation
-int Puzzle::h(heuristicFunction hFunction){
-
+int Puzzle::h(heuristicFunction hFunction) {
 	int sum=0;
 	int h=0;
 	int numOfMisplacedTiles=0;
 
-	switch(hFunction){
+	switch(hFunction) {
 		case misplacedTiles:
-				//place your implementation here
-                h = numOfMisplacedTiles;
+						//place your implementation here
+            h = numOfMisplacedTiles;
 		        break;
 
 		case manhattanDistance:
 		        //place your implementation here
-
 		        h = sum;
 		        break;
 
 	};
-
 	return h;
-
 }
 
 
 //converts board state into its string representation
-string Puzzle::toString(){
-  int n;
+string Puzzle::toString() {
+  int n = 0;
   string stringPath;
-
-  n=0;
-  for(int i=0; i < 3; i++){
-		for(int j=0; j < 3; j++){
+  for(int i=0; i < 3; i++) {
+		for(int j=0; j < 3; j++) {
 		    stringPath.insert(stringPath.end(), board[i][j] + '0');
 		    n++;
 		}
   }
-
 //  cout << "toString = " << stringPath << endl;
-
   return stringPath;
 }
 
-bool Puzzle::goalMatch(){
-	for(int i=0; i < 3; i++){
-		for(int j=0; j < 3; j++){
+string Puzzle::getString() {
+	return strBoard;
+}
+
+bool Puzzle::goalMatch() {
+	for(int i=0; i < 3; i++) {
+		for(int j=0; j < 3; j++) {
 			if (board[i][j] != goalBoard[i][j]) return false;
 		}
   }
 	return true;
 }
 
-bool Puzzle::canMoveLeft(){
-   return (x0 > 0);
-
-}
-
-const string Puzzle::getPath(){
+const string Puzzle::getPath() {
 	return path;
 }
 
-bool Puzzle::canMoveRight(){
+bool Puzzle::canMoveLeft() {
+   return (x0 > 0);
+}
 
+bool Puzzle::canMoveRight() {
    return (x0 < 2);
-
 }
 
-bool Puzzle::canMoveUp(){
-
+bool Puzzle::canMoveUp() {
    return (y0 > 0);
-
 }
 
-bool Puzzle::canMoveDown(){
-
+bool Puzzle::canMoveDown() {
    return (y0 < 2);
-
 }
 
 ///////////////////////////////////////////////
 //these functions will be useful for Progressive Deepening Search
 
-bool Puzzle::canMoveLeft(int maxDepth){
+bool Puzzle::canMoveLeft(int maxDepth) {
   	//put your implementations here
 }
 
-bool Puzzle::canMoveRight(int maxDepth){
+bool Puzzle::canMoveRight(int maxDepth) {
    //put your implementations here
 }
 
-bool Puzzle::canMoveUp(int maxDepth){
+bool Puzzle::canMoveUp(int maxDepth) {
    //put your implementations here
 }
 
-bool Puzzle::canMoveDown(int maxDepth){
+bool Puzzle::canMoveDown(int maxDepth) {
    //put your implementations here
 }
 
 ///////////////////////////////////////////////
 
-Puzzle *Puzzle::moveLeft(){
-
+Puzzle *Puzzle::moveLeft() {
 	Puzzle *p = new Puzzle(*this);
-
-
    if(x0 > 0) {
-
 		p->board[y0][x0] = p->board[y0][x0-1];
 		p->board[y0][x0-1] = 0;
-
 		p->x0--;
-
 		p->path = path + "L";
 		p->pathLength = pathLength + 1;
 		p->depth = depth + 1;
-
-
 	}
 	p->strBoard = p->toString();
 
 	return p;
-
 }
 
-Puzzle *Puzzle::moveRight(){
-
+Puzzle *Puzzle::moveRight() {
    Puzzle *p = new Puzzle(*this);
-
-
-   if(x0 < 2){
-
+   if(x0 < 2) {
 		p->board[y0][x0] = p->board[y0][x0+1];
 		p->board[y0][x0+1] = 0;
-
 		p->x0++;
-
 		p->path = path + "R";
 		p->pathLength = pathLength + 1;
-
 		p->depth = depth + 1;
-
 	}
-
 	p->strBoard = p->toString();
 
 	return p;
-
 }
 
-Puzzle *Puzzle::moveUp(){
-
+Puzzle *Puzzle::moveUp() {
    Puzzle *p = new Puzzle(*this);
-
-
-   if(y0 > 0){
-
+   if(y0 > 0) {
 		p->board[y0][x0] = p->board[y0-1][x0];
 		p->board[y0-1][x0] = 0;
-
 		p->y0--;
-
 		p->path = path + "U";
 		p->pathLength = pathLength + 1;
-
 		p->depth = depth + 1;
-
 	}
 	p->strBoard = p->toString();
 
 	return p;
-
 }
 
-Puzzle *Puzzle::moveDown(){
-
+Puzzle *Puzzle::moveDown() {
    Puzzle *p = new Puzzle(*this);
-
-
-   if(y0 < 2){
-
+   if(y0 < 2) {
 		p->board[y0][x0] = p->board[y0+1][x0];
 		p->board[y0+1][x0] = 0;
-
 		p->y0++;
-
 		p->path = path + "D";
 		p->pathLength = pathLength + 1;
-
 		p->depth = depth + 1;
-
 	}
 	p->strBoard = p->toString();
 
 	return p;
-
 }
 
 /////////////////////////////////////////////////////
 
-
-void Puzzle::printBoard(){
+void Puzzle::printBoard() {
 	cout << "board: "<< endl;
 	for(int i=0; i < 3; i++){
 		for(int j=0; j < 3; j++){
@@ -302,9 +250,8 @@ void Puzzle::printBoard(){
 		}
 	}
 	cout << endl;
-
 }
 
-int Puzzle::getPathLength(){
+int Puzzle::getPathLength() {
 	return pathLength;
 }
